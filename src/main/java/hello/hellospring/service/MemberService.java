a7d77bdfe9907b0b57bbe72a7a806d80a6f39027
kyh1126 <kyh901126@nate.com>
2020-10-03 18:01:12 +0900

    AOP Chapter 1. AOP가 필요한 상황 - 시간 측정용 기능을 서비스에 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,11 +25,16 @@ public class MemberService {
 	 * 회원 가입
 	 */
 	public Long join(Member member) {
-		// 같은 이름이 있는 중복 회원X
-		validateDuplicateMember(member);
-
-		memberRepository.save(member);
-		return member.getId();
+		long start = System.currentTimeMillis();
+		try {
+			validateDuplicateMember(member); //중복 회원 검증
+			memberRepository.save(member);
+			return member.getId();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("join " + timeMs + "ms");
+		}
 	}
 
 	private void validateDuplicateMember(Member member) {
@@ -40,7 +45,14 @@ public class MemberService {
 	 * 전체 회원 조회
 	 */
 	public List<Member> findMembers() {
-		return memberRepository.findAll();
+		long start = System.currentTimeMillis();
+		try {
+			return memberRepository.findAll();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("findMembers " + timeMs + "ms");
+		}
 	}
 
 	public Optional<Member> findOne(Long memberId) {
